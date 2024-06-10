72f3f24120d5a03f470d43c694f1242bf9ec01c6
Kichiro <tiobloody@gmail.com>
2024-06-09 21:26:07 -0300

    Finalização da Pesquisa binária

--- javaswing/src/main/java/modelos/PesquisaBinaria.java
+++ javaswing/src/main/java/modelos/PesquisaBinaria.java
@@ -1,54 +1,26 @@
 package modelos;
-import java.util.Scanner;
 
 public class PesquisaBinaria extends Pesquisa {
-	
-	    public static int binarySearch(int[] array, int target) {
-	        int left = 0;
-	        int right = array.length - 1;
 
-	        while (left <= right) {
-	            int mid = left + (right - left) / 2;
+	public static int binarySearch(long[] array, long target) {
+        int left = 0;
+        int right = array.length - 1;
 
-	            if (array[mid] == target) {
-	                return mid;
-	            }
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
 
-	            if (array[mid] < target) {
-	                left = mid + 1;
-	            } else {
-	         
-	                right = mid - 1;
-	            }
-	        }
+            if (array[mid] == target) {
+                return mid;
+            }
 
-	        return -1;
-	    }
+            if (array[mid] < target) {
+                left = mid + 1;
+            } else {
 
-	    public static void main(String[] args) {
-	        Scanner scanner = new Scanner(System.in);
+                right = mid - 1;
+            }
+        }
 
-	        System.out.print("Digite o tamanho do array: ");
-	        int size = scanner.nextInt();
-
-	        int[] array = new int[size];
-	        System.out.println("Digite os elementos do array em ordem crescente:");
-	        for (int i = 0; i < size; i++) {
-	            array[i] = scanner.nextInt();
-	        }
-
-	        System.out.print("Digite o elemento alvo a ser pesquisado: ");
-	        int target = scanner.nextInt();
-
-	        int result = binarySearch(array, target);
-	        if (result == -1) {
-	            System.out.println("Elemento não encontrado no array");
-	        } else {
-	            System.out.println("Elemento encontrado no índice: " + result);
-	        }
-
-	        scanner.close();
-	    }
-	}
-		
-	    	
+        return -1;
+    }
+}
