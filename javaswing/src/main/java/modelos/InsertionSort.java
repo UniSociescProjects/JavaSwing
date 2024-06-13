f46b52bbf876fa03242a2b31a2c454371fc693e5
João Janz <85969062+joaovictorjanz@users.noreply.github.com>
2024-06-13 17:56:14 -0300

    Update InsertionSort.java

--- javaswing/src/main/java/modelos/InsertionSort.java
+++ javaswing/src/main/java/modelos/InsertionSort.java
@@ -2,4 +2,17 @@ package modelos;
 
 public class InsertionSort extends Ordenacao {
 
+    public static void sort(long[] array) {
+        int n = array.length;
+        for (int i = 1; i < n; ++i) {
+            int key = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = key;
+        }
+    }
 }
